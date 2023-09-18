dff1c3549f529c96120fc33a78c983315d48e663
CristianMejia2198 <3004917940101@ingenieria.usac.edu.gt>
2023-09-18 10:49:47 -0600

    Clase 8 - Arbol B

--- Clase8/nodoB.py
+++ Clase8/nodoB.py
@@ -1,6 +1,7 @@
 class NodoB():
-    def __init__(self, valor): #Agregar los valores que necesitan
+    def __init__(self, valor, id): #Valor sera el numero final del ascii, y id sera el id de la tarea
         self.valor = valor
+        self.id = id
         self.siguiente = None
         self.anterior = None
         self.izquierda = None
