001b91ff97dcec13f6f295af9c9991069286d913
CristianMejia2198 <3004917940101@ingenieria.usac.edu.gt>
2023-09-17 20:38:13 -0600

    Clase 8 - Arbol B

---
+++ Clase8/nodoB.py
@@ -0,0 +1,7 @@
+class NodoB():
+    def __init__(self, valor): #Agregar los valores que necesitan
+        self.valor = valor
+        self.siguiente = None
+        self.anterior = None
+        self.izquierda = None
+        self.derecha = None
